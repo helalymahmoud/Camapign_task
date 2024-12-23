06990105e4bdd977d74a10d2d4d014a5bca1483e
mahmoud <mahmoud.hisham.7370@gmail.com>
2024-12-23 12:35:33 +0200

    back

--- src/data.ts
+++ src/data.ts
@@ -1,32 +1,32 @@
-// import { Ad } from './ads/entities/ads.entity';
-// import { Campaign } from './campaigns/entities/campaign.entity';
+import { Ad } from './ads/entities/ads.entity';
+import { Campaign } from './campaigns/entities/campaign.entity';
 
 
-// export const campaigns: Campaign[] = [
-//   {
-//       id: "1", name: 'John', description: "fref",
-//       startDate: undefined,
-//       endDate: undefined,
-//       status: 'd',
-//       ads: [],
-//       partners: [],
-//       users: []
-//   },
+export const campaigns: Campaign[] = [
+  {
+      id: "1", name: 'John', description: "fref",
+      startDate: undefined,
+      endDate: undefined,
+      status: 'd',
+      ads: [],
+      partners: [],
+      users: []
+  },
 
-// ];
+];
 
-// export const ads: Ad[] = [
-//   {
-//       id: "1", name: 'Sam', studentId: 1,
-//       campaigned: 'gh',
-//       title: 'gf',
-//       content: 'fh',
-//       type: 'fh',
-//       status: 'fh',
-//       campaigns: [],
-//       campaignId: "0",
-//       users:[],
-//       partners:[],
-//   },
+export const ads: Ad[] = [
+  {
+      id: "1", name: 'Sam', studentId: 1,
+      campaigned: 'gh',
+      title: 'gf',
+      content: 'fh',
+      type: 'fh',
+      status: 'fh',
+      campaigns: [],
+      campaignId: "0",
+      users:[],
+      partners:[],
+  },
 
-// ];
+];
